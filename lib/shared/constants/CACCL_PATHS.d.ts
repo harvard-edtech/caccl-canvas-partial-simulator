c2234f66a1515461e2d9bf200a87fa896ecb9133
Gabe Abrams <gabeabrams@gmail.com>
2022-03-07 18:49:00 -0500

    Updated paths

--- lib/shared/constants/CACCL_PATHS.d.ts
+++ lib/shared/constants/CACCL_PATHS.d.ts
@@ -1,12 +1,14 @@
-declare const _default: {
-    LAUNCH: string;
-    AUTHORIZE: string;
-    SELF_LAUNCH: string;
-    FORWARDER_PREFIX: string;
-    HOME: string;
-};
 /**
  * CACCL paths shared across the whole project
  * @author Gabe Abrams
  */
-export default _default;
+declare const CACCL_PATHS: {
+    LAUNCH: string;
+    AUTHORIZE: string;
+    SELF_LAUNCH: string;
+    FORWARDER_PREFIX: string;
+    STATUS: string;
+    HANDLE_PASSBACK: string;
+    HOME: string;
+};
+export default CACCL_PATHS;
